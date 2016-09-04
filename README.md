1b89e8e7270b10319c8364f972062510c0583311
Michael Scire <SciresM@gmail.com>
2016-09-04 03:35:51 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+Homebrew to display which type of screens (IPS or TN) your N3DS has.
+
+Does not work on O3DS.
---
+++ source/main.c
@@ -0,0 +1,71 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <3ds.h>
+
+int main(int argc, char **argv)
+{
+    // Initialize services
+    aptInit();
+    gfxInitDefault();
+    hidInit();
+    ptmSysmInit();
+    gspLcdInit();
+    
+    consoleInit(GFX_TOP, NULL);
+    if (PTMSYSM_CheckNew3DS())
+    {
+        u8 Screens = (u8)GSPLCD_GetVendor();
+        printf("\nScreen Info:\n\n");
+        switch ((Screens >> 4) & 0xF)
+        {
+            case 1:
+                printf("Upper screen: IPS\n");
+                break;
+            case 0xC:
+                printf("Upper screen: TN\n");
+                break;
+            default:
+                printf("Upper screen: Unknown\n");
+                break;
+        }
+         switch (Screens & 0xF)
+        {
+            case 1:
+                printf("Lower screen: IPS\n");
+                break;
+            case 0xC:
+                printf("Lower screen: TN\n");
+                break;
+            default:
+                printf("Lower screen: Unknown\n");
+                break;
+        }
+    }
+    else
+    {
+        printf("\nCan't check O3DS Screen vendors.\n");
+    }
+    
+    printf("\nPress START to exit.\n");
+    
+    while(aptMainLoop()) //Always have the main code loop in here
+    {
+        hidScanInput(); //Checks which keys are pressed
+
+        u32 button = hidKeysDown(); // Checks which keys are up and which ones are down
+        if (button & KEY_START)
+        {
+            CFG_UpdateConfigNANDSavegame();
+            break;
+        }
+    } 
+
+    // Exit services
+    aptExit();
+    gfxExit();
+    hidExit();
+    ptmSysmExit();
+    gspLcdExit();
+    return 0;
+}
